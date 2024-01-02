c3348484c26681fc1dbf2726ecbbda30abe102bc
Woongkyu Jee <woong.jee.16@ucl.ac.uk>
2024-01-02 18:24:39 +0000

    mos_analyser - extracting HOMO/LUMO

---
+++ Extractor/FHIaims/sorting_test.py
@@ -0,0 +1,11 @@
+data = [
+    {'eval': 3, 'elem': [1, 2, 3, 4, 5]},
+    {'eval': 2, 'elem': [2, 5, 1, 3, 7]},
+    {'eval': 5, 'elem': [9, 2, 3, 4, 5]},
+    {'eval': 1, 'elem': [9, 2, 3, 4, 5]},
+    {'eval': 0, 'elem': [9, 2, 3, 4, 5]}
+]
+
+sorted_data = sorted(data, key=lambda x: x['eval'])
+
+print(sorted_data)
