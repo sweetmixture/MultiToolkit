e9cf342ff7822f8853eb86f2da3587db403b9b3e
Woongkyu Jee <woong.jee.16@ucl.ac.uk>
2024-02-05 22:03:28 +0000

    cannonical ensemble novib xrd/rdf

---
+++ Extractor/utils_byProjects/MnO-Li/MnO_XRD/CE_analysis/CE_NoVib/ce_analysis_xrd.py
@@ -0,0 +1,386 @@
+#!/bin/python
+
+import os,sys
+import pandas as pd
+import numpy as np
+import pickle
+
+import time
+#current_time = time.time()
+#new_time = time.time()
+#time_difference = new_time - current_time
+#print("Current Time:", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_time)))
+#print("Time Difference (seconds):", time_difference)
+#print(f' ! loading at ',time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ct)))
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+
+# from concurrent.futures import ProcessPoolExecutor
+
+# constants ----
+_wavenumber_to_ev = np.float128(1.239841984332e-4)
+_kb = np.float128(8.617333262e-5)
+
+#
+# internal use functions	-----------------------------------------------------
+#
+def expkbt(E,T):
+
+	global _kb
+	#E = np.float128(E)
+	#T = np.float128(T)
+
+	#try:
+	#	ret = np.exp(-E/T/_kb)
+	#except OverflowError:
+	#	if -E/T/_kb > 500:
+	#		ret = np.exp(500.)
+
+	ret = np.exp(-E/T/_kb)
+	return ret
+
+def expkbt_obs(E,T,O):
+
+	global _kb
+	ret = np.float128(0.)
+	E = np.float128(E)
+	T = np.float128(T)
+
+	ret = O * np.exp(-E/T/_kb)
+
+	return ret
+
+def get_gz(csvdf,T,vib=False,pkldf=None):
+
+	global _kb
+	global _wavenumber_to_ev
+	#u = np.float128(0.)	# _corr
+
+	Elist = csvdf['energy'].values
+	Egm = np.float128(Elist[0])		# global minimum
+
+	T = np.float128(T)
+	Z = np.float128(0.)
+	G = np.float128(0.)
+
+	if vib == False:
+
+		for E in Elist:
+			E = np.float128(E) - Egm
+			Z += expkbt(E,T)
+		
+		Z = np.exp(-Egm/_kb/T) * Z	
+		G = - _kb * T * np.log(Z)
+		#print(Egm,-Egm/_kb/T,np.exp(-Egm/_kb/T),Z,G)
+	#
+	# include vibrational contributions
+	#
+	elif vib == True:
+		#
+		# looping thru structures / distinguished by 'taskid'
+		#
+		for E,taskid in zip(Elist,csvdf['taskid'].values):
+
+			E = np.float128(E) - Egm
+			ZE = expkbt(E,T)
+
+			#
+			# get freq info for this struct
+			#
+			freqlist = np.array(pkldf[taskid],dtype=np.float128)
+			# add ZPE contribution - gamma point ?
+			freq0_power = np.float128(0.)
+			for freq in freqlist:
+				freq0_power += (0.5 * freq * _wavenumber_to_ev)	
+			Zvib0 = np.exp(-freq0_power/_kb/T)
+	
+			# add all vib contribution
+			# NotImplemented
+
+			# sum up
+			Z += (ZE * Zvib0)
+
+		# pull back Egm
+		Z = np.exp(-Egm/_kb/T) * Z
+		G = - _kb * T * np.log(Z)
+
+	return np.float128(G),np.float128(Z)
+
+
+
+
+
+
+# USER DEF
+
+freq_path = '/work/e05/e05/wkjee/Masters/Zirui2023/MnO/conpshell/freq_pkl'					# frequency files path
+xrd_path = '/work/e05/e05/wkjee/Masters/Zirui2023/MnO/conpshell/xrd_pkl'
+std_path = '/work/e05/e05/wkjee/Masters/Zirui2023/MnO/conpshell/xrd_pkl'
+
+# custom functions
+
+# load any pkl file
+def load_pkl(pklfile):
+	return pd.read_pickle(pklfile)
+
+
+# ---------------- output
+
+print(f' ! canonical analysis xrd ---------')
+print(f' * freq_path pkl form			 : {freq_path}')
+print(f' * xrd_path  pkl form			 : {xrd_path}')
+print(f' * std_path gulp_std_output(csv) : {std_path}')
+print(f' ! Extrating info ...',flush=True)
+
+freq_pkl_list = [ f'freq{i}.pkl' for i in range(25) ]		# DV - dummy variables
+xrd_pkl_list = [ f'xrd{i}.pkl' for i in range(25) ]			# DV
+std_csv_list = [ f'nconp{i}.csv' for i in range(25) ]		# DV
+
+freq_pkl_paths = [os.path.join(freq_path, item) for item in freq_pkl_list]
+xrd_pkl_paths = [os.path.join(xrd_path, item) for item in xrd_pkl_list]
+std_csv_paths = [os.path.join(std_path, item) for item in std_csv_list]
+
+# ----------------- file check
+fchecklist = []
+for file in freq_pkl_paths:
+	fchecklist.append(os.path.exists(file))
+if False not in fchecklist:
+	print(f' * freq_pkl file check done')
+else:
+	print(f' * freq_pkl file extracting failed')
+	sys.exit(1)
+fchecklist = []
+for file in xrd_pkl_paths:
+	fchecklist.append(os.path.exists(file))
+if False not in fchecklist:
+	print(f' * xrd_pkl	file check done')
+else:
+	print(f' * xrd_pkl file extracting failed')
+	sys.exit(1)
+fchecklist = []
+for file in std_csv_paths:
+	fchecklist.append(os.path.exists(file))
+if False not in fchecklist:
+	print(f' * std_csv	file check done')
+else:
+	print(f' * std_csv file extracting failed')
+	sys.exit(1)
+print(f' ! * * *',flush=True)
+
+# ------------------- data loading
+ct1 = time.time()
+
+# Function to load CSV file
+def load_csv(std):
+	return pd.read_csv(std)
+
+# Function to load pickle file
+def load_pickle(freq_pkl):
+	return pd.read_pickle(freq_pkl)
+
+std_dflist = []
+freq_dflist = []
+xrd_dflist = []
+
+# Parallelize loading of CSV files
+print(f' ! starting at ', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), flush=True)
+print(f' ! reading in std_csv', end='')
+with ProcessPoolExecutor() as executor:
+	#futures_csv = [executor.submit(load_csv, std) for std in tqdm(std_csv_paths, desc="load gulp stdout csv", total=len(std_csv_paths))]
+	futures_csv = [executor.submit(load_csv, std) for std in std_csv_paths]
+	for future in tqdm(futures_csv, desc='Loading gulp stdout csv', total=len(std_csv_paths)):
+		std_df = future.result()
+		std_dflist.append(std_df)
+print(f' - loading at ', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), flush=True)
+#for csv in std_dflist:
+#	print(csv)
+
+# Parallelize loading of pickle files for freq
+print(f' ! starting at ', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), flush=True)
+print(f' ! reading in freq_pkl', end='')
+with ProcessPoolExecutor() as executor:
+	#futures_freq = [executor.submit(load_pickle, freq_pkl) for freq_pkl in tqdm(freq_pkl_paths, desc='load gulp freq pkl', total=len(freq_pkl_paths))]
+	futures_freq = [executor.submit(load_pickle, freq_pkl) for freq_pkl in freq_pkl_paths]
+	for future in tqdm(futures_freq, desc='Loading gulp freq pkl', total=len(freq_pkl_paths)):
+		freq_df = future.result()
+		freq_dflist.append(freq_df)
+print(f' - loading at ', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), flush=True)
+
+ct2 = time.time()
+load_et = ct2 - ct1
+print(f' * loading elapsed time : {load_et} (s)',flush=True)
+print(f' ! ------------------------------------------------------------------------')
+
+
+#
+# PREPARATION PHASE 1 - DROP IMAG FREQ SAMPLES
+#
+imlog = open("imag_freqlist.txt", "w") 
+imlog.write(' * imag frequency listing [taskid]\n')
+
+for i, (std_df,pkl_df) in enumerate(zip(std_dflist,freq_dflist)):		# 'i' is for file index !! not structure index
+
+	taskid_list = std_df['taskid'].tolist()
+
+	print(f' * processing size {i} | data count {len(std_df)} ...')
+	_drop_cnt = 0
+	imag_freqlist = []
+
+	# 1st scan : looping imag freq
+	for struct_no, taskid in enumerate(taskid_list):
+	
+		# get frequencies of a struct with 'taskid'
+		freqlist = pkl_df[taskid]
+
+		_isdropped = False
+
+		for freq in freqlist:
+			if freq < -0.5:
+				imag_freqlist.append(taskid)
+
+				std_df = std_df.drop(struct_no)
+				_isdropped = True
+				_drop_cnt = _drop_cnt + 1
+				break
+
+		if not _isdropped:
+			pkl_df[taskid] = pkl_df[taskid][3:]
+			
+	imlog.write(f' * {_drop_cnt}/{len(taskid_list)} structures with imaginary frequencies were found : ')
+	for imag_taskid in imag_freqlist:
+		imlog.write(f' {imag_taskid},')
+	imlog.write('\n')
+
+	print(f' * {_drop_cnt}/{len(taskid_list)} structures with imaginary frequencies were found')
+	print(f' ! finished')
+	
+	std_dflist[i] = std_df		# after dropping out imag freq taskids
+	freq_dflist[i] = pkl_df		# after dropping out imag freq taskids -> here freq_dflist[i] is with trans/rota freqencies
+
+imlog.close() # close imag freq log
+#
+#
+# ------------------------ imag freq structure dropping finished
+
+
+#
+# PREPARATION PHASE 2 - Energy Recalibration
+#
+
+# some model parameters
+_e_gulp_mn2O4 = -534.25669638*6.										# R - Mn24O48 energy - unitcell based -534.25669638
+_LiOx		  = +5.39171												# Li(g) -> Li(+)(g) + e-
+_MnRe		  = -51.2													# Mn(4+)(g) + e- -> Mn(3+)(g)
+# this may change
+_corr		  = +1.6510 + 5.7400										# 1.6510(eV): formation energy Li CRC : +159.3kJ/mol Li(g), + residual energy 5.740 (eV) # _corr = 7.391
+# using sample count
+_max_sample = 10000
+sizelist = [ i for i in range(25) ]
+
+
+tmp_csvlist = []		# processed csvlist
+for s, std_df in zip(sizelist,std_dflist):
+
+	if len(std_df) > 10000:
+		#csvdf = csvdf.sample(n=_max_sample, random_state=42)	# use same randseed to keep reproducibility
+		std_df = std_df.head(10000)
+
+	# Lithiation Reaction Energy converting
+	std_df['energy'] = (std_df['energy'] - _e_gulp_mn2O4 + (_LiOx + _MnRe + _corr) * s )/24.
+	#std_df['energy'] = (std_df['energy'] - _e_gulp_mn2O4 + (_LiOx + _MnRe + 0. ) * s )/24. # no chemical potential
+	std_df = std_df.sort_values(by='energy',ascending=True)
+
+	print(f' * reaction energy calculation ... size : {s}')
+	#print(std_df)
+	tmp_csvlist.append(std_df)
+
+std_dflist = tmp_csvlist
+
+# ---- Setting Temperature
+try:
+	_T = np.float128(float(sys.argv[1]))
+except:
+	_T = np.float128(300.)
+try:
+	_vib_flag = sys.argv[2]
+	if _vib_flag == '-vib':
+		_include_vib = True
+except:
+	_include_vib = False
+
+print(f' ! ------------------------------------------------------------------------')
+print(f' * start ensemble analysis')
+print(f' * temperature (K)     = {_T}')
+print(f' * including vibration = {_include_vib}')
+
+#
+# G,Z =  def get_gz(csvdf,T,vib=False):
+#
+npGlist = np.zeros(len(std_dflist),dtype=np.float128)
+npZlist = np.zeros(len(std_dflist),dtype=np.float128)
+
+for s, (std_df,freq_df) in enumerate(zip(std_dflist,freq_dflist)):
+
+	#print(f' * processing partition function Z : size {s}')
+	Gc,Zc = get_gz(std_df,_T,vib=_include_vib,pkldf=freq_df)		# MUST INCLUDE VIB FOR X = 0 Otherwise -> ERRRRRORRRRR!!!
+	npGlist[s] = Gc
+	npZlist[s] = Zc
+	print(npGlist[s],npZlist[s])
+
+print(f' ! ------------------------------------------------------------------------')
+print(f' * calculation of canonical G / Z done ...')
+print(f' ! ------------------------------------------------------------------------')
+
+#
+# Perform XRD Canonical analysis
+#
+
+# xrd_pkl_paths
+
+# loading twotheta value
+xrd_df = load_pkl(xrd_pkl_paths[0])
+ttheta = xrd_df[0]['twotheta']
+xrd_ce_intslist = []	# for using later
+del xrd_df
+
+for size, std_df in enumerate(std_dflist):
+
+	print(f' * processing {size} xrd ...')	
+
+	xrd_df = load_pkl(xrd_pkl_paths[size])
+	xrd_ce_ints = [ 0. for i in range(len(ttheta)) ]	# prepare empty ints
+
+	# for this size / samples
+
+	Elist = std_df['energy'].values
+	Egm = np.float128(Elist[0])		# global minimum
+
+	taskidlist = std_df['taskid'].values
+
+	for k, (E,taskid) in enumerate(zip(Elist,taskidlist)):
+
+		print(f" ! working on : {k}/{len(taskidlist)}", end='\r')
+
+		xrd_data = xrd_df[taskid]			# get xrd for sample 'taskid'
+		xrd_ints = xrd_data['intensity']	# extract intensity for this sample
+
+		E = np.float128(E) - Egm
+
+		# extract individual intensity
+		for i, ints in enumerate(xrd_ints):
+
+			xrd_ce_ints[i] += np.float128(ints) * expkbt(E,_T)
+	
+	xrd_ce_ints_nptmp = np.array(xrd_ce_ints)
+	xrd_ce_ints_nptmp = xrd_ce_ints_nptmp * np.exp(-Egm/_kb/_T) / npZlist[size]
+
+	xrd_ce_ints = xrd_ce_ints_nptmp.tolist()
+	xrd_ce_intslist.append(xrd_ce_ints)
+
+	print(f' ! writing xrd output ...')
+
+	with open(f'XRD_{_T}_{size}.xrd','w') as f:
+		for tt,ints  in zip(ttheta,xrd_ce_ints):
+			f.write('%20.12e%20.12e\n' % (tt,ints))
+
+	del xrd_df
---
+++ Extractor/utils_byProjects/MnO-Li/MnO_rdfPostProcessingScripts/CE_analysis/CE_NoVib_Plot/plot_ce_RDF_m3.py
@@ -0,0 +1,112 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+
+# Function to plot data with offset and Gaussian broadening
+
+# Example usage
+_T   = 10
+_max = 24
+
+offset = 6
+offset = 12
+sigma = 3
+
+'''
+	color scheme
+'''
+c_1 = 'lime'		# LiLi	(3)
+c_2 = 'darkorchid'	# TcTc	(4)
+c_3 = 'slategrey'	# LiTc	(7)
+
+def plot_data(file_paths, offset=offset, broadening_sigma=sigma):
+
+	global _T
+
+	'''
+		plot config
+	'''
+	cm = 1/2.54
+	fig, axes = plt.subplots(1,3)
+	fig.set_size_inches((32*cm,16*cm))
+
+	plt.subplots_adjust(
+		left = 0.04,
+		bottom = 0.1,
+		right = 0.96,
+		top = 0.96,
+		wspace = 0.200,
+		hspace = 0.0
+	)
+	
+	# font size
+	_fs = 12
+
+	axes[0].set_xlabel('Li$^{+}$-Li$^{+}$ (Å)', fontsize=_fs)
+	axes[1].set_xlabel('Mn$^{3+}$-Mn$^{3+}$ (Å)', fontsize=_fs)
+	axes[2].set_xlabel('Mn$^{3+}$-Li$^{+}$ (Å)', fontsize=_fs)
+
+	for i,ax in enumerate(axes):
+		ax.tick_params(axis='x', labelsize=_fs)  # Corrected line
+		#ax.tick_params(axis='y', labelsize=_fs)  # Corrected line
+		ax.set_xlim(1.5, 8.0)
+
+		ax.xaxis.set_major_locator(MultipleLocator(1))
+		ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+
+		ax.set_yticks([])
+		if i == 0:
+			ax.set_ylabel('RDF (a.u.)', fontsize=_fs)
+	
+	# ----------------------------------------------------------
+
+	for i, file_path in enumerate(file_paths):
+		# Read data from file
+		with open(file_path, 'r') as file:
+			#data = np.loadtxt(file, skiprows=2)
+			data = np.loadtxt(file)
+			x = data[:, 0]
+
+			y1 = data[:, 2]
+			y2 = data[:, 3]
+			y3 = data[:, 6]
+ 
+		# Normalise
+		#max_y1 = max(y1)
+		#max_y2 = max(y2)
+		#max_y3 = max(y3)
+		max_y1 = 1
+		max_y2 = 1
+		max_y3 = 1
+		#print(max_y1,max_y2,max_y3)
+		y_normalised1 = y1 / max_y1
+		y_normalised2 = y2 / max_y2
+		y_normalised3 = y3 / max_y3
+	   
+		# Apply offset to y values
+		y_offset = i * offset
+		
+		# Apply Gaussian broadening to y values
+		#y_broadened = gaussian_filter1d(y, sigma=broadening_sigma)
+		y_broadened1 = gaussian_filter1d(y_normalised1, sigma=broadening_sigma)
+		y_broadened2 = gaussian_filter1d(y_normalised2, sigma=broadening_sigma)
+		y_broadened3 = gaussian_filter1d(y_normalised3, sigma=broadening_sigma)
+		
+		# Plot data
+		axes[0].plot(x, y_broadened1 + y_offset, label=file_path, color=c_1)
+		axes[1].plot(x, y_broadened2 + y_offset, label=file_path, color=c_2)
+		axes[2].plot(x, y_broadened3 + y_offset, label=file_path, color=c_3)
+
+	plt.show()
+
+	fig.savefig(f'rdf_ce_{_T}.png', dpi=1200, bbox_inches='tight')
+	fig.savefig(f'rdf_ce_{_T}.pdf', format='pdf', dpi=1200, bbox_inches='tight')
+
+
+#file_paths = [ f'RDF_{_T}.0_{i}.rdf' for i in range(_max) ]
+file_paths = [ f'RDF_{_T}.0_{(i+1)*3}.rdf' for i in range(8) ]
+
+
+
+plot_data(file_paths)
